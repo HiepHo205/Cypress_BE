cdf17313e35de17ffc284eacf69e1a8499e91d4b
Bang-2108 <zoanthibang@gmail.com>
2026-07-16 15:55:59 +0700

    feat: add frontend components and update Vite configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-//
+import './bootstrap';
+import { createApp } from 'vue';
+import AdminDashboard from './components/AdminDashboard.vue';
+
+const app = createApp({});
+
+app.component('admin-dashboard', AdminDashboard);
+app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
-
+import vue from '@vitejs/plugin-vue'; 
 export default defineConfig({
     plugins: [
         laravel({
@@ -15,8 +15,29 @@ export default defineConfig({
             ],
         }),
         tailwindcss(),
+        vue({ 
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
-    server: {
+    resolve: { 
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js',
+            '@': '/resources/js',
+        },
+    },
+   server: {
+        host: '0.0.0.0', 
+        port: 5173,      
+        cors: true,      
+        origin: 'http://localhost:5173',
+        hmr: {
+            host: 'admin.cypresshub.com', 
+        },
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
